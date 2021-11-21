9ae5e2ac303da34886191013f84dc6c4ac6a19fc
Michael Kreil <github@michael-kreil.de>
2021-11-21 21:50:59 +0100

    Update update-subtrees.sh

--- update-subtrees.sh
+++ update-subtrees.sh
@@ -1,38 +1,21 @@
 #!/bin/bash
 
-cd "$(dirname "$0")"
+dir="$(dirname "$0")"
+cd "${dir}"
 
 set -e
 
 function update() {
 	repo="$1"
-	remote="repo-$repo"
 
-	echo $repo
+	cd "${dir}"
+	cd "../${repo}"
 
-	if git remote get-url "${remote}" 2>&1 > /dev/null; then
-		# ok
-		echo "   - has remote"
-	else
-		echo "   - add remote"
-		git remote add "${remote}" "git@github.com:nachtrag-de/${repo}.git"
-	fi
+	git pull
+	cd "${dir}"
+	mkdir "docs/${repo}/"
 
-	echo "   - add subtree"
-	git subtree add -P "docs/${repo}" "${remote}" main
-
-	echo "   - pull"
-	git subtree pull -P "docs/${repo}" "${remote}" main
-
-	echo "   finished"
+	rsync --delete "../${repo}/docs/" "docs/${repo}/"
 }
 
-if [[ $(git status --porcelain=v1 2> /dev/null) ]]; then
-	echo "ERROR: git has changes"
-	exit
-fi
-
 update "covpassfake"
-
-# git remote add covpassfake	git@github.com:nachtrag-de/covpassfake.git
-# git subtree add -P docs/covpassfake covpassfake main
